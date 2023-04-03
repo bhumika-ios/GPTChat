131f9136d54291ddce756091c61593f66e92b169
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-04-03 17:10:36 +0530

    setting view perform setting with gptchat version default for display - used radio button used , model select version gpt , temperature for used stepper and display gpt key and suncroniseand custom prompt

--- GPTChat/ContentView.swift
+++ GPTChat/ContentView.swift
@@ -1,88 +1,176 @@
+////
+////  ContentView.swift
+////  GPTChat
+////
+////  Created by Bhumika Patel on 03/04/23.
+////
 //
-//  ContentView.swift
-//  GPTChat
+//import SwiftUI
 //
-//  Created by Bhumika Patel on 03/04/23.
+//struct ContentView: View {
+////    @Environment(\.managedObjectContext) private var viewContext
+////
+////    @FetchRequest(
+////        sortDescriptors: [NSSortDescriptor(keyPath: \DialogueData.date, ascending: false)],
+////        animation: .default)
+////    private var items: FetchedResults<DialogueData>
+////    
+////    @StateObject var configuration = AppConfiguration.shared
+////    @State var dialogueSessions: [DialogueSession] = []
+////    @State var selectedDialogueSession: DialogueSession?
+////    
+////    @State var isShowSettingView = false
+////    
+////    @State var isReplying = false
+////    
+////    @State private var columnVisibility = NavigationSplitViewVisibility.doubleColumn
 //
-
-import SwiftUI
-import CoreData
-
-struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \DialogueData.date, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<DialogueData>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.date!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.date!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
-}
+//    var body: some View {
+//        NavigationSplitView(columnVisibility: $columnVisibility) {
+//            contentView()
+//                .toolbar {
+//#if os(iOS)
+//                    ToolbarItem(placement: .automatic) {
+//                        Button {
+//                            isShowSettingView = true
+//                        } label: {
+//                            Image(systemName: "ellipsis.circle")
+//                        }
+//                    }
+//#endif
+//                    ToolbarItem(placement: .automatic) {
+//                        Button {
+//                            addItem()
+//                        } label: {
+//                            Image(systemName: "square.and.pencil")
+//                        }
+//                    }
+//                }
+//        } detail: {
+//            ZStack {
+//                if let selectedDialogueSession = selectedDialogueSession {
+//                    MessageListView(session:selectedDialogueSession)
+//                        .onReceive(selectedDialogueSession.$isReplying.didSet) { isReplying in
+//                            self.isReplying = isReplying
+//                        }
+//                        .onReceive(selectedDialogueSession.$conversations.didSet) { conversations in
+//                            if conversations.isEmpty {
+//                                isReplying = true
+//                                isReplying = false
+//                            }
+//                        }
+//                }
+//            }
+//#if os(iOS)
+//            .navigationBarTitleDisplayMode(.inline)
+//            .toolbarBackground(.visible, for: .navigationBar)
+//#else
+//            .frame(minWidth: 500)
+//#endif
+//        }
+//        .navigationSplitViewStyle(.balanced)
+//#if os(macOS)
+//        .frame(minWidth: 800, minHeight: 500)
+//        .background(.secondarySystemBackground)
+//#else
+//        .sheet(isPresented: $isShowSettingView) {
+//            NavigationStack {
+//                AppSettingsView(configuration: configuration)
+//                    .navigationBarTitleDisplayMode(.inline)
+//                    .toolbar {
+//                        ToolbarItem {
+//                            Button {
+//                                isShowSettingView = false
+//                            } label: {
+//                                Text("Done")
+//                                    .bold()
+//                            }
+//                        }
+//                    }
+//            }
+//        }
+//#endif
+//        .onAppear() {
+//            dialogueSessions = items.compactMap {
+//                DialogueSession(rawData: $0)
+//            }
+//        }
+//
+//
+//    }
+//    
+//    
+////    @ViewBuilder
+////    func contentView() -> some View {
+////        if dialogueSessions.isEmpty {
+////            DialogueListPlaceholderView()
+////        } else {
+////            DialogueSessionListView(
+////                dialogueSessions: $dialogueSessions,
+////                selectedDialogueSession: $selectedDialogueSession,
+////                isReplying: $isReplying
+////            ) {
+////                deleteItems(offsets: $0)
+////            } deleteDialogueHandler: {
+////                deleteItem($0)
+////            }
+////        }
+////    }
+//
+//
+////    private func addItem() {
+////        withAnimation {
+////            do {
+////                let session = DialogueSession()
+////                dialogueSessions.insert(session, at: 0)
+////                let newItem = DialogueData(context: viewContext)
+////                newItem.id = session.id
+////                newItem.date = session.date
+////                newItem.configuration =  try JSONEncoder().encode(session.configuration)
+////                try PersistenceController.shared.save()
+////            } catch {
+////                // Replace this implementation with code to handle the error appropriately.
+////                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+////                let nsError = error as NSError
+////                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+////            }
+////        }
+////    }
+//
+////    private func deleteItems(offsets: IndexSet) {
+////        withAnimation {
+////            dialogueSessions.remove(atOffsets: offsets)
+////            offsets.map { items[$0] }.forEach(viewContext.delete)
+////
+////            do {
+////                try PersistenceController.shared.save()
+////            } catch {
+////                // Replace this implementation with code to handle the error appropriately.
+////                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+////                let nsError = error as NSError
+////                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+////            }
+////        }
+////    }
+////
+////    private func deleteItem(_ session: DialogueSession) {
+////        withAnimation {
+////            dialogueSessions.removeAll {
+////                $0.id == session.id
+////            }
+////            if let item = session.rawData {
+////                viewContext.delete(item)
+////            }
+////
+////            do {
+////                try PersistenceController.shared.save()
+////            } catch {
+////                // Replace this implementation with code to handle the error appropriately.
+////                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+////                let nsError = error as NSError
+////                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+////            }
+////        }
+////    }
+//    
+//}
---
+++ GPTChat/View/Setting/AppSettingsView.swift
@@ -0,0 +1,178 @@
+//
+//  AppSettingsView.swift
+//  GPTChat
+//
+//  Created by Bhumika Patel on 03/04/23.
+//
+import SwiftUI
+
+class AppConfiguration: ObservableObject {
+    
+    static let shared = AppConfiguration()
+    
+    @AppStorage("configuration.key") var key = "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"
+    
+    @AppStorage("configuration.model") var model: OpenAIModelType = .chatgpt {
+        didSet {
+            if !model.supportedModes.contains(mode) {
+                mode = model.supportedModes.first!
+            }
+        }
+    }
+    
+    @AppStorage("configuration.mode") var mode: Mode = .chat
+    
+    @AppStorage("configuration.temperature") var temperature: Double = 0.5
+    
+    @AppStorage("configuration.systemPrompt") var systemPrompt: String = "You are a helpful assistant"
+    
+    @AppStorage("configuration.isMarkdownEnabled") var isMarkdownEnabled: Bool = false
+    
+}
+
+extension OpenAIModelType: RawRepresentable {
+    var group: OpenAIModelGroup {
+        switch self {
+        case .textDavinci:
+            return .gpt3
+        case .textCurie:
+            return .gpt3
+        case .textBabbage:
+            return .gpt3
+        case .textAda:
+            return .gpt3
+        case .codeDavinci:
+            return .codex
+        case .codeCushman:
+            return .codex
+        case .textDavinciEdit:
+            return .feature
+        case .chatgpt:
+            return .chat
+        case .chatgpt0301:
+            return .chat
+        }
+    }
+}
+
+enum OpenAIModelGroup: String, CaseIterable, Codable, Identifiable {
+    case chat = "Chat"
+    case gpt3 = "GPT3"
+    case codex = "Codex"
+    case feature = "Feature"
+    
+    var id: RawValue {
+        rawValue
+    }
+    
+    var models: [OpenAIModelType] {
+        switch self {
+        case .chat:
+            return OpenAIModelType.chatModels
+        case .gpt3:
+            return OpenAIModelType.gpt3Models
+        case .codex:
+            return OpenAIModelType.codexModels
+        case .feature:
+            return OpenAIModelType.featureModels
+        }
+    }
+}
+
+struct AppSettingsView: View {
+    
+    @ObservedObject var configuration: AppConfiguration
+    
+    @State private var selectedGroup = OpenAIModelGroup.chat
+    let groups = [OpenAIModelGroup.chat, .gpt3, .codex]
+    
+    @State private var selectedModel = OpenAIModelType.chatgpt
+    @State var models: [OpenAIModelType] = OpenAIModelType.chatModels
+    
+    @State private var selectedMode = Mode.chat
+    @State var modes = OpenAIModelType.chatgpt.supportedModes
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        Form {
+            Section(header: "Display") {
+                HStack {
+                    Toggle("Markdown Enabled", isOn: $configuration.isMarkdownEnabled)
+                    Spacer()
+                }
+            }
+            Section(header: "Model") {
+                HStack {
+                    Text("Model")
+                        .fixedSize()
+                    Spacer()
+                    Picker("Model", selection: $selectedModel) {
+                        ForEach(models, id: \.self) { model in
+                            Text(model.rawValue)
+                                .tag(model)
+                        }
+                    }
+                    .labelsHidden()
+                    .onChange(of: selectedModel, perform: updateModes(_:))
+                }
+                VStack {
+                    Stepper(value: $configuration.temperature, in: 0...1, step: 0.1) {
+                        HStack {
+                            Text("Temperature")
+                            Spacer()
+                            Text(String(format: "%.1f", configuration.temperature))
+                                .padding(.horizontal)
+                                .height(32)
+                                .width(60)
+                                .background(Color.secondarySystemFill)
+                                .cornerRadius(8)
+                        }
+                    }
+                }
+                HStack {
+                    Image(systemName: "key")
+                    Spacer()
+                    TextField("", text: $configuration.key)
+                        .truncationMode(.middle)
+                }
+            }
+            Section("Prompt") {
+                NavigationLink {
+                    PromptsListView()
+                } label: {
+                    Text("Sync Prompts")
+                }
+                NavigationLink {
+                    CustomPromptsView()
+                } label: {
+                    Text("Custom Prompts")
+                }
+            }
+        }
+        .onAppear() {
+            self.selectedGroup = configuration.model.group
+            self.selectedModel = configuration.model
+            self.selectedMode = configuration.mode
+        }
+        .navigationTitle("Settings")
+    }
+    
+    
+    private func updateModes(_ model: OpenAIModelType) {
+        configuration.model = model
+        modes = model.supportedModes
+        selectedMode = modes.first!
+    }
+}
+
+
+struct AppSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            AppSettingsView(configuration: AppConfiguration())
+        }
+    }
+}
+
